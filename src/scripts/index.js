212da9fab7dc569e4a6de2d41df4967efdc76e8b
Giulio Nisi <email@giulionisi.it>
2025-03-22 08:52:14 +0100

    Some frontend reviews.

---
+++ src/scripts/index.js
@@ -0,0 +1,41 @@
+const API_URL = "http://localhost:8080/api/"
+
+function getPolesData() {
+    const POLES_DATA_URL = API_URL + "poles_data"
+    
+    fetch(POLES_DATA_URL)
+        .then(response => response.json())
+            .then(data => {
+                let unordered_list = document.getElementsByTagName("ul")[0]
+
+                let poles_data_list = data["poles_data"]
+                if (poles_data_list === undefined) {
+                    console.error('Error in getting data inside the APIs.');
+                    let p_error = document.getElementsByTagName("p")[0]
+                    p_error.textContent = "Errore nel reperire i dati all'interno delle APIs."
+                    return
+                }
+
+                poles_data_list.forEach(pole_obj => {
+                    let li = document.createElement("li")
+
+                    let a = document.createElement("a")
+                    let pole_name = Object.keys(pole_obj)[0].toLowerCase().trim()
+                    a.href = "/polo.html?polo=" + pole_name
+                    a.textContent = pole_name.charAt(0).toUpperCase() + pole_name.slice(1)
+
+                    li.appendChild(a)
+
+                    unordered_list.appendChild(li)
+                });
+            })
+    .catch(error => {
+        console.error('Error in fetching APIs:', error);
+        let p_error = document.getElementsByTagName("p")[0]
+        p_error.textContent = "Errore nel contattare le APIs."
+    })
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    getPolesData()
+});
